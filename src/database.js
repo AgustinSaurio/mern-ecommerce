43fe95bd489293ae774b2de99d18bd8c25a99cb6
Agustin <agustincaceresjuega@gmail.com>
2022-09-25 04:05:31 -0300

    test api resolve

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-      await mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.bchbyio.mongodb.net/${MONGO_DB}?retryWrites=true&w=majority`);
+      await mongoose.connect("mongodb+srv://" + MONGO_USER + ":" + MONGO_PASSWORD + "@cluster0.bchbyio.mongodb.net/" + MONGO_DB + "?retryWrites=true&w=majority");
     } catch (error) {
       console.error(error);
     }
